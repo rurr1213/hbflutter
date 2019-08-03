248b949a5a4e6a211a50aa19f512b93296d894f6
rrajapakse <rrajapakse@bfrx.com>
2019-08-03 12:10:31 -0700

    initial workign app

---
+++ src/flutter_app/lib/main.dart
@@ -0,0 +1,289 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: 'Homebase application'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+//                  color: Colors.blue,
+/*
+                  constraints: BoxConstraints(
+                      maxWidth: 300.0,
+                      minWidth: 200.0,
+                      maxHeight: 200.0,
+                      minHeight: 150.0
+                  ),
+*/
+                  child: new Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      //mainAxisSize: MainAxisSize.max,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        new Row( // top menu
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                               //new LoginForm(
+                               new MyTestForm(
+                               ),
+                            ]
+                        ),
+                        new Row( // main body
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[ new Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.max,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  new Text(
+                                    'You have pushed this button this many times Ravi2:',
+                                    style: new TextStyle(fontSize: 12.0,
+                                        color: const Color(0xFF000000),
+                                        fontWeight: FontWeight.w200,
+                                        fontFamily: "Roboto"),
+                                  ),
+                                  new Text(
+                                    '$_counter',
+                                    style: Theme
+                                        .of(context)
+                                        .textTheme
+                                        .display1,
+                                  ),
+                                ]
+                            )
+                            ]
+                        )
+                      ]
+                  )
+              ),
+            ],
+          ),
+        ),
+      drawer: Drawer(
+        elevation: 16.0,
+        child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountName: Text("xyz"),
+              accountEmail: Text("xyz@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Text("xyz"),
+              ),
+              otherAccountsPictures: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text("abc"),
+                )
+              ],
+            ),
+            ListTile(
+              title: new Text("All Inboxes"),
+              leading: new Icon(Icons.mail),
+            ),
+            Divider(
+              height: 0.1,
+            ),
+            ListTile(
+              title: new Text("Primary"),
+              leading: new Icon(Icons.inbox),
+            ),
+            ListTile(
+              title: new Text("Social"),
+              leading: new Icon(Icons.people),
+            ),
+            ListTile(
+              title: new Text("Promotions"),
+              leading: new Icon(Icons.local_offer),
+            )
+          ],
+        ),
+      ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incrementCounter,
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
+        ),
+        // This trailing comma makes auto-formatting nicer for build methods.
+        persistentFooterButtons: <Widget>[
+          RaisedButton(
+            elevation: 10.0,
+            onPressed: () {},
+            color: Colors.blueGrey,
+            child: Icon(
+              Icons.clear,
+              color: Colors.white,
+            ),
+          ),
+          RaisedButton(
+            elevation: 10.0,
+            onPressed: () {},
+            child: Icon(
+              Icons.add,
+              color: Colors.white,
+            ),
+            color: Colors.green,
+          ),
+        ],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          fixedColor: Colors.teal,
+          items: [
+            BottomNavigationBarItem(
+              title: Text("Home"),
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              title: Text("Search"),
+              icon: Icon(Icons.search),
+            ),
+            BottomNavigationBarItem(
+              title: Text("Add"),
+              icon: Icon(Icons.add_box),
+            ),
+          ],
+          onTap: (int index) {
+            setState(() {
+           //   currentIndex = index;
+            });
+          },
+        ),
+    );
+  }
+}
+
+
+class MyTestForm extends StatefulWidget {
+  MyTestForm({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyTestFormState createState() => _MyTestFormState();
+}
+
+class _MyTestFormState extends State<MyTestForm> {
+  int _counter = 0;
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Row(
+            children: <Widget>[
+          Container(
+                constraints: BoxConstraints(
+                    maxHeight: 300.0,
+                    maxWidth: 200.0,
+                    minWidth: 150.0,
+                    minHeight: 150.0
+                ),
+                child: Column(
+                  children: <Widget> [
+                  TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: RaisedButton(
+                      onPressed: () {
+                        // Validate returns true if the form is valid, or false
+                        // otherwise.
+                        if (_formKey.currentState.validate()) {
+                          // If the form is valid, display a Snackbar.
+                          Scaffold.of(context)
+                              .showSnackBar(SnackBar(content: Text('Processing Data')));
+                        }
+                      },
+                      child: Text('Submit'),
+                    ),
+                    ),
+                  ],
+                ),
+          ),],
+        ),
+    );
+  }
+}
